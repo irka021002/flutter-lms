d9e2510fcd002f13c12b36055812f691f142143a
irka021002 <irfankamal021002@gmail.com>
2024-07-25 11:44:52 +0700

    first commit

---
+++ lib/splash_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/web_view.dart';
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const WebView()),
+        );
+      },
+      child: Image.asset(
+        'assets/splash.jpg',
+        width: double.infinity,
+      ),
+    );
+  }
+}
